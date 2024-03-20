3b1ca25fe23deb8e439a3331e5fca136e36a65bd
WaelZ01 <149936969+WaelZ01@users.noreply.github.com>
2024-03-20 21:07:54 +0000

    skills

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const menuIcon = document.querySelector('.menu-icon');
-//     const navLinks = document.querySelector('.nav-links');
-
-//     menuIcon.addEventListener('click', function () {
-//         navLinks.classList.toggle('active');
-//     });
-// });
-
-
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+
+
+// let progress = document.querySelectorAll('.prgress span')
+//     spans = document.querySelectorAll('.progress-title span')
+// progress.forEach((prog)=>{
+//     prog.style.width = prog.dataset.width;
+// });
+// spans.forEach((span)=>{
+// span.style.left = span.dataset.num;
+// span.style.opacity.visibility = 'visible'
+// })
+
+// Get all progress bars
+const progressBars = document.querySelectorAll('.progress-bar .progress span');
+
+// Animate each progress bar to its specified width
+progressBars.forEach(progress => {
+    const width = progress.dataset.width;
+    progress.style.width = width;
+});
+
+
+
